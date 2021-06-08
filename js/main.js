c941cc70f13e9f5d562c5e0ed1b6bd4043c78441
Forrest Herman <58410716+forrest-herman@users.noreply.github.com>
2021-06-07 20:19:39 -0400

    basic update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+;(function () {
+    // Loading page
+    var loaderPage = function () {
+        $(".loader").fadeOut("slow")
+    }
+
+    $(function () {
+        // goToTop()
+        loaderPage()
+    })
+})()
